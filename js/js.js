3a17a578a71375463f5fa518968d7ff18db37e74
noireko <agustinfernandezrolon@gmail.com>
2026-09-16 20:56:05 -0300

    Created starting files with API fetch function

---
+++ js/js.js
@@ -0,0 +1,11 @@
+async function weatherAppAPI() {
+    try {
+        const res = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5");
+        const data = await res.json();
+        console.log(data.timezone);
+    } catch (err) {
+        console.log(err);
+    }   
+}
+
+weatherAppAPI();
