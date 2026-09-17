89a60e88a77c7d10ef9a55bd92d072523a18bccf
noireko <agustinfernandezrolon@gmail.com>
2026-09-17 12:05:17 -0300

    Select city

--- js/js.js
+++ js/js.js
@@ -1,9 +1,8 @@
-const ubicacion = document.getElementById("ubicacion");
+const ubicacion = document.getElementById("ubicacion").value;
 
 async function weatherAppAPI() {
-    let ubi = ubicacion.value;
     try {
-        const res = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5");
+        const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ubicacion}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
         const data = await res.json();
         console.log(data.timezone);
     } catch (err) {
