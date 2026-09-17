685abb33df805dddf9089167f4ee5bf44e0e5db0
noireko <agustinfernandezrolon@gmail.com>
2026-09-17 12:23:43 -0300

    Fixed weatherApp async function

--- js/js.js
+++ js/js.js
@@ -1,14 +1,17 @@
-const ubicacion = document.getElementById("ubicacion").value;
+const boton = document.getElementById("boton");
 
-async function weatherAppAPI() {
-    try {
-        const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${ubicacion}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
-        const data = await res.json();
-        console.log(data.timezone);
-    } catch (err) {
-        console.log(err);
-    }   
-}
+boton.addEventListener("click", function () {
+    const valorInput = document.getElementById("ubicacion").value;
+    async function weatherApp() {
+        try {
+            const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${valorInput}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
+            const data = await res.json()
+            console.log(data.timezone)
+        } catch (err) {
+            console.log(err);
+        };
+    };
+    weatherApp();
+});
 
-weatherAppAPI();
 
