b539dbe545aa38c31ad23897492bc7ba9e079f10
noireko <agustinfernandezrolon@gmail.com>
2026-09-16 21:10:13 -0300

    agregue variables

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 const ubicacion = document.getElementById("ubicacion");
 
 async function weatherAppAPI() {
+    let ubi = ubicacion.value;
     try {
         const res = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London%2CUK?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5");
         const data = await res.json();
