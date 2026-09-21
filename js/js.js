1af093a734a05ed0c109cb80e32b691a56aeb06d
noireko <agustinfernandezrolon@gmail.com>
2026-09-21 18:35:21 -0300

    guardado

--- js/js.js
+++ js/js.js
@@ -6,7 +6,7 @@ boton.addEventListener("click", function () {
         try {
             const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${valorInput}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
             const data = await res.json()
-            console.log(data.timezone)
+            console.log(data.timezone);
         } catch (err) {
             console.log(err);
         };
