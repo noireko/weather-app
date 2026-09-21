1d485c2be036ac4de6cbf36e8b348152d107d5e7
noireko <agustinfernandezrolon@gmail.com>
2026-09-21 19:31:33 -0300

    Created function for showing weather data

--- js/js.js
+++ js/js.js
@@ -1,7 +1,12 @@
 const boton = document.getElementById("boton");
+let weatherData;
 
 function mostrarDatos() {
-    const 
+    const divGeneral = document.getElementById("contenedor-general");
+    const miDiv = document.createElement("div");
+    miDiv.setAttribute("id", "data-weather");
+    divGeneral.appendChild(miDiv);
+    miDiv.innerHTML = weatherData;
 }
 
 boton.addEventListener("click", function () {
@@ -9,8 +14,10 @@ boton.addEventListener("click", function () {
     async function weatherApp() {
         try {
             const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${valorInput}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
-            const data = await res.json()
-            console.log(data.timezone);
+            const data = await res.json();
+            weatherData = data.timezone;
+            console.log(weatherData);
+            mostrarDatos();
         } catch (err) {
             console.log(err);
         };
