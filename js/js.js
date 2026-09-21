0a938ea776f27b98c213674973a04931fdf34013
noireko <agustinfernandezrolon@gmail.com>
2026-09-21 19:59:38 -0300

    Added showDailyData function

--- js/js.js
+++ js/js.js
@@ -6,8 +6,18 @@ function mostrarDatos() {
     const miDiv = document.createElement("div");
     miDiv.setAttribute("id", "data-weather");
     divGeneral.appendChild(miDiv);
-    miDiv.innerHTML = weatherData;
+    miDiv.innerHTML = `
+        <h2>${weatherData.ciudad}</h2>
+        <p>Temperatura: ${weatherData.temp}°F</p>
+        <p>Sensación térmica: ${weatherData.sensacion}°F</p>
+        <p>Condición: ${weatherData.descripcion}</p>
+        <p>Humedad: ${weatherData.humedad}%</p>
+        <p>Viento: ${weatherData.viento} mph</p>
+        <p>Amanecer: ${weatherData.amanecer}</p>
+        <p>Atardecer: ${weatherData.atardecer}</p>
+    `;
 }
+
 
 boton.addEventListener("click", function () {
     const valorInput = document.getElementById("ubicacion").value;
@@ -15,7 +25,19 @@ boton.addEventListener("click", function () {
         try {
             const res = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${valorInput}%2C?unitGroup=us&key=AD2XUC4M9DXJY3KNG5CFGBZU5`);
             const data = await res.json();
-            weatherData = data.timezone;
+            const actual = data.currentConditions;
+            weatherData = {
+                ciudad: data.resolvedAddress,
+                temp: Math.round(actual.temp),
+                sensacion: Math.round(actual.feelslike),
+                descripcion: actual.conditions,
+                icono: actual.icon,
+                humedad: actual.humidity,
+                viento: actual.windspeed,
+                amanecer: actual.sunrise,
+                atardecer: actual.sunset,
+            };
+
             console.log(weatherData);
             mostrarDatos();
         } catch (err) {
